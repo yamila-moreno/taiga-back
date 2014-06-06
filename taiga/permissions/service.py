ba2293df468861d388c3fff3ed638d7b975486a0
Jesús Espino <jesus.espino@kaleidos.net>
2014-06-06 13:29:42 +0200

    WIP

--- taiga/permissions/service.py
+++ taiga/permissions/service.py
@@ -23,16 +23,20 @@ def user_has_perm(user, perm, obj=None):
     if not project:
         return False
 
-    role = _get_user_project_role(user, project)
-    if role:
-        return role_has_perm(role, perm)
-    elif project.is_private:
-        return False
-    elif user.is_authenticated():
-        return perm in project.anon_permissions + project.public_permissions
-    else:
-        return perm in project.anon_permissions
+    return perm in get_user_project_permissions(user, project)
 
 
 def role_has_perm(role, perm):
     return perm in role.permissions
+
+
+def get_user_project_permissions(user, project):
+    role = _get_user_project_role(user, project)
+    if role:
+        return role.permissions
+    elif project.is_private:
+        return []
+    elif user.is_authenticated:
+        return project.anon_permissions + project.public_permissions
+    else:
+        return project.anon_permissions
